98da64081408c921967890553fe0e571ce104809
Michael Trullas Garcia <adoser2@hotmail.es>
2026-04-20 00:50:12 +0200

    wip

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testEnvironment: 'jest-environment-jsdom',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
