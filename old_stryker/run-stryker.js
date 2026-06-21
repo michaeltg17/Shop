07bd522247f6501476555c36e45ea4f2822c47a4
Michael Trullas Garcia <adoser2@hotmail.es>
2026-06-21 22:47:54 +0200

    ui and api

--- old_stryker/run-stryker.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-  execSync('npx stryker run', { stdio: 'inherit' });
-} finally {
-  execSync('node stop-server.js', { stdio: 'inherit' });
-}
